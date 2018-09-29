eaa3ab44c3bf167ec030822c1a8ba2c2dbd28123
matteo <matteo.lacki@gmail.com>
2018-09-29 11:31:57 +0200

    Something adds a big bump to the spectrum... How the hell?

--- masstodon/scripts/PXD001845/strange_orbitrap_behaviour.py
+++ masstodon/scripts/PXD001845/strange_orbitrap_behaviour.py
@@ -9,25 +9,39 @@ from masstodon.scripts.PXD001845.get_fasta import read_fasta
 from masstodon.scripts.PXD001845.hecks_custom_ptms import modify_fasta
 from masstodon.scripts.PXD001845.hecks_spectrum import get_hecks_spectrum
 
-all_data = "/Users/matteo/Projects/masstodon/data/PXD001845/numpy_files"
-folder   = "20141202_AMB_Bora_10x_40MeOH_1FA_OT_120k_10uscans_971_ETD_4ms_18precZ"
-scan     = 1
-mz, intensity = get_hecks_spectrum(all_data, folder, scan)
+# common
+all_data  = "/Users/matteo/Projects/masstodon/data/PXD001845/numpy_files"
+main_dump = "/Users/matteo/Projects/masstodon/masstodon/dump/debug"
+folder    = "20141202_AMB_Bora_10x_40MeOH_1FA_OT_120k_10uscans_971_ETD_4ms_18precZ"
 
-original_fasta 		 = read_fasta(folder)
-fasta, modifications = modify_fasta(original_fasta)
+def fit(scan_no):
+	mz, intensity = get_hecks_spectrum(all_data, folder, scan_no)
+	original_fasta 		 = read_fasta(folder)
+	fasta, modifications = modify_fasta(original_fasta)
 
-charge            = 18
-min_prob          = .8
-isotopic_coverage = .999
-std_cnt           = 3
+	charge            = 18
+	min_prob          = .8
+	isotopic_coverage = .999
+	std_cnt           = 3
 
-m, tm = masstodon_single(mz, intensity, fasta, charge,
-	                     orbitrap 		  	= True,
-	                     isotopic_coverage 	= isotopic_coverage,
-	                     min_prob 			= min_prob, 
-	                     std_cnt  			= std_cnt,
-	                     get_timings       	= True)
+	m, tm = masstodon_single(mz, intensity, fasta, charge,
+		                     orbitrap 		  	= True,
+		                     isotopic_coverage 	= isotopic_coverage,
+		                     min_prob 			= min_prob, 
+		                     std_cnt  			= std_cnt,
+		                     get_timings       	= True)
 
-m.plotlygl("/Users/matteo/Projects/masstodon/masstodon/dump")
-m.dump("dump")
+	m_dump = pjoin(main_dump, str(1))
+	return m, tm, m_dump
+
+
+m_scan = 1
+m, tm, m_dump = fit(m_scan)
+m.plotlygl(m_dump)
+
+
+n_scan = 2
+n, tn, n_dump = fit(n_scan)
+n.plotlygl(n_dump)
+
+
