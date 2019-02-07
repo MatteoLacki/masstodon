68c906570be953ee0db799a8fedd7ac17a312e5c
matteo <matteo.lacki@gmail.com>
2019-02-07 09:19:09 +0100

    version tag updata

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ with open("README.md", "r") as fh:
 setup(
     name='masstodon',
     packages=find_packages(),
-    version='0.15',
+    version='0.16',
     description='Investigate mass spectra for chemical substances, especially ETD products.',
     long_description=long_description,
     long_description_content_type="text/markdown",
